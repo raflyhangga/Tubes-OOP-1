07fcecc2e16c2352b17e35d6157b31f879bbe9fb
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-04-02 05:59:31 +0700

    feat: added toko and item toko header
    Moved building header into tubesoop1 folder

---
+++ include/tubesoop1/toko/itemtoko.h
@@ -0,0 +1,13 @@
+#ifndef ITEM_TOKO_H
+#define ITEM_TOKO_H
+using namespace std;
+#include "tubesoop1/resource/resource.h"
+
+class ItemToko {
+    private:
+        Resource &item;
+        int jumlah;
+};
+
+
+#endif
---
+++ include/tubesoop1/toko/toko.h
@@ -0,0 +1,15 @@
+#ifndef TOKO_H
+#define TOKO_H
+#include "tubesoop1/toko/itemtoko.h"
+#include <vector>
+using namespace std;
+
+
+class Toko {
+    private:
+        vector<ItemToko> inventory;
+
+};
+
+
+#endif
