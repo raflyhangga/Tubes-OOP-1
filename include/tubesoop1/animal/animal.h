8c2e0ad3ed8f6e4cd5608c048e89fdffca6588d2
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-04-05 15:57:12 +0700

    feat: abstraction animal

--- include/tubesoop1/animal/animal.h
+++ include/tubesoop1/animal/animal.h
@@ -2,21 +2,92 @@
 #define ANIMAL_H
 
 #include <string>
+#include "tubesoop1/resource/resource.h"
+#include "tubesoop1/product/product.h"
+#include "tubesoop1/plant/plant.h"
+#include <vector>
 using namespace std;
 
 // <ID> <KODE_HURUF> <NAME> <TYPE> <WEIGHT_TO_HARVEST> <PRICE>
-class Animal
+class Animal: public Resource
 {
 private:
-    string kode_huruf;
-    string name;
     int weightToHarvest;
-    int price;
 
 public:
-    virtual string getType() = 0;
     friend ostream &operator<<(ostream &os, const Animal &a);
     friend istream &operator>>(istream &is, Animal &a);
+};
+
+class Herbivora: public Animal {
+    public:
+        virtual void makan(Plant&);
+        virtual void makan(Product&);
+};
+
+class Karnivora: public Animal {
+    public:
+        virtual void makan(Animal&);
+        virtual void makan(Product&);
+};
+
+class Omnivora: public Animal {
+    public:
+        virtual void makan(Animal&);
+        virtual void makan(Plant&);
+        virtual void makan(Product&);
+};
+
+class Cow:public Herbivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+class Sheep:public Herbivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+class Horse:public Herbivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+class Rabbit:public Herbivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+
+class Snake:public Karnivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+
+class Chicken:public Omnivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
+};
+class Duck:public Omnivora{
+    private:
+        static string kode_huruf;
+        static string name;
+        static int price;
+        static vector<Resource&> hasil_panen;
 };
 
 #endif
