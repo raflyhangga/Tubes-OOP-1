6ab078a63d50575a1f2cf56dbfa10d90f52187c2
RayhanFadhlan <rayhanfazka07@gmail.com>
2024-04-06 16:14:36 +0700

    fix product

--- src/product/product.cpp
+++ src/product/product.cpp
@@ -2,7 +2,6 @@
 
 Product::Product(string code, string name, int price, int addedWeight) : Resource(code, name, price)
 {
-    this->origin = origin;
     this->addedWeight = addedWeight;
 }
 
@@ -10,34 +9,28 @@ Product::~Product()
 {
 }
 
-string Product::getOrigin() const
-{
-    return origin;
-}
+
 
 int Product::getAddedWeight() const
 {
     return addedWeight;
 }
 
-void Product::setOrigin(string origin)
-{
-    this->origin = origin;
-}
+
 
 void Product::setAddedWeight(int addedWeight)
 {
     this->addedWeight = addedWeight;
 }
 
-ProductAnimal::ProductAnimal(string id, string code, string name, int price, string origin, int addedWeight) : Product(code, name, price, addedWeight){
+ProductAnimal::ProductAnimal(string code, string name, int price, int addedWeight) : Product(code, name, price, addedWeight){
     
 }
 
-ProductFruit::ProductFruit(string id, string code, string name, int price, string origin, int addedWeight) : Product(code, name, price, addedWeight){
+ProductFruit::ProductFruit(string code, string name, int price, int addedWeight) : Product(code, name, price, addedWeight){
     
 }
 
-ProductMaterial::ProductMaterial(string id, string code, string name, int price, string origin, int addedWeight) : Product(code, name, price, addedWeight){
+ProductMaterial::ProductMaterial(string code, string name, int price, int addedWeight) : Product(code, name, price, addedWeight){
     
 }
