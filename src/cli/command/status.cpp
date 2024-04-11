250e4612e238978144d752ae1c5c6a2b1128f9e6
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-04-11 14:48:30 +0700

    nit: also add winning condition to status

--- src/cli/command/status.cpp
+++ src/cli/command/status.cpp
@@ -19,9 +19,14 @@ void Status::execute(Petani *player) {
 }
 
 void Status::execute(Walikota *player) {
+    cout << "================== [Player Info] =====================" << endl;
     cout << "Nama:  " << player->getUsername() << endl;
     cout << "Role:  Walikota" << endl;
     cout << "Uang:  " << player->getMoney() << endl;
     cout << "Berat: " << player->getWeight() << endl;
+    cout << "=============== [Winning Condition] ==================" << endl;
+    cout << "Berat untuk menang: " << Player::weightToWin << endl;
+    cout << "Uang untuk menang: " << Player::moneyToWin << endl;
+    cout << "======================================================" << endl;
 }
 
